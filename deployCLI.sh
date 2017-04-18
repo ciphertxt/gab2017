0a229fd018050f7c68a90b79467b39525071e63f
Scott Hoag <scottmhoag@gmail.com>
2017-04-18 11:38:58 -0400

    Add files via upload

---
+++ deployCLI.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# parameters
+
+resourceGroupName="gab2017CLI"
+resourceGroupLocation="eastus2"
+vmName="cliVM"
+vmSize="Standard_D1_v2"
+
+# Login to the service
+#login to azure using your credentials
+az account show 1> /dev/null
+
+if [ $? != 0 ];
+then
+	az login
+fi
+
+# set account
+az account set --subscription "MSDNOld"
+
+# Create a resource group
+az group create --name $resourceGroupName --location $resourceGroupLocation
+
+az vm create --resource-group $resourceGroupName --name $vmName --image win2016datacenter --admin-username cliadmin --admin-password 'cliadminDevise!!!'
